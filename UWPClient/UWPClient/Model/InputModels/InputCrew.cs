736860457392835a7241d6eb8f5b096c959011a9
Dmytro <dima.smolts@gmail.com>
2018-07-29 12:24:04 +0300

    some input models added

---
+++ UWPClient/UWPClient/Model/InputModels/InputCrew.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPClient.Model.InputModels
+{
+    public class InputCrew
+    {
+		public int PilotId { get; set; }
+		public int[] StewardessIds { get; set; }
+	}
+}
